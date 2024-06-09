9e3b97c5d9e21fe84572e05a6885aead0401ced2
Baku-Stark <wysthellyjessemata13@gmail.com>
2024-06-09 18:26:44 -0300

    Update : `BootCamp - Python (Docker)`

--- API/workout_api/centro_treinamento/__init__.py
+++
@@ -1,3 +0,0 @@
-"""
-Centro de Treinamento
-"""
--- API/workout_api/contrib/__init__.py
+++ API/workout_api/contrib/__init__.py
@@ -1,3 +1,7 @@
-from workout_api.categorias.models import CategoriaModel
-from workout_api.centro_treinamento.models import CentroTreinamentoModel
-from workout_api.atleta.models import AtletaModel
+"""
+    `contrib`
+
+    [-] dependencies.py
+    [-] models.py
+    [-] schemas.py
+"""
