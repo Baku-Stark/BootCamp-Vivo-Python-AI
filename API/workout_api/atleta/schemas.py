e5c91e647906fc58782f9fba2ff5d9ee6f8d3acd
Baku-Stark <wysthellyjessemata13@gmail.com>
2024-06-12 22:50:51 -0300

    Update : `BootCamp - Python (Criação das rotas de Atleta)`

--- API/workout_api/atleta/schemas.py
+++ API/workout_api/atleta/schemas.py
@@ -25,5 +25,5 @@ class AtletaOut(Atleta, OutMixin):
     pass
 
 class AtletaUpdate(BaseSchema):
-    nome: Annotated[Optional[str], Field(None, description='Nome do atleta', example='Joao', max_length=50)]
+    nome: Annotated[Optional[str], Field(None, description='Nome do atleta', example='Wallace', max_length=50)]
     idade: Annotated[Optional[int], Field(None, description='Idade do atleta', example=25)]
